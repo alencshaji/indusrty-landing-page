bc5d2a67af25ee84ee09920983d81af44bdaad26
alencshaji <alencshaji@gmail.com>
2024-11-02 21:04:09 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('show');
+}
+
+function tryProduct() {
+    alert("Redirecting to product trial page...");
+}
+
+function watchDemo() {
+    alert("Playing demo video...");
+}
